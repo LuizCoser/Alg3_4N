19b3bcdce8853a65badb1d1bdbfef03565104067
Luiz Coser <Luiz Coser@LuizCoser.home>
2013-09-29 22:27:07 -0300

    Trabalho II

---
+++ ExerciciosAula/TrabalhoII/TrabalhoII/src/Cor.java
@@ -0,0 +1,28 @@
+public class Cor {
+
+	private int red;
+	private int green;
+	private int blue;
+	
+	
+	public Cor(int red, int green, int blue) {
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+
+
+	public int getRed() {
+		return red;
+	}
+
+	public int getGreen() {
+		return green;
+	}
+
+	public int getBlue() {
+		return blue;
+	}
+
+
+}
